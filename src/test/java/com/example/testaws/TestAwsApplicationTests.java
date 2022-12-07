3dca4457aa9e67fb1ab3d25f9c908a7c66aeefe1
Jeremy Benoit <jeremy.benoit@student.vinci.be>
2022-12-07 14:27:53 +0100

    init

---
+++ src/test/java/com/example/testaws/TestAwsApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.testaws;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAwsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+  @Test
+  public void assetHelloWorld() {
+    assertEquals("hello","hel");
+  }
+}
